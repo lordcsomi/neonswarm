c06a13d0d154db76d7e01890f780a6d68816736a
Csomi <csomos.adam.benedek@gmail.com>
2024-11-11 23:51:21 +0100

    bullets and enemy spawning initial

--- game/bullet.go
+++ game/bullet.go
@@ -1,11 +1,11 @@
 components {
-  id: "square"
-  component: "/game/square.script"
+  id: "bullet"
+  component: "/game/bullet.script"
 }
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"square\"\n"
+  data: "default_animation: \"line\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
@@ -16,21 +16,19 @@ embedded_components {
     z: 1.0
   }
   scale {
-    x: 2.0
-    y: 2.0
-    z: 2.0
+    x: 0.05
+    y: 0.1
   }
 }
 embedded_components {
-  id: "square_collisionobject"
+  id: "collisionobject"
   type: "collisionobject"
   data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"enemy\"\n"
-  "mask: \"tower\"\n"
-  "mask: \"bullet\"\n"
+  "group: \"bullet\"\n"
+  "mask: \"enemy\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -41,8 +39,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 49.0\n"
-  "  data: 49.0\n"
+  "  data: 11.0\n"
+  "  data: 2.5\n"
   "  data: 10.0\n"
   "}\n"
   ""
